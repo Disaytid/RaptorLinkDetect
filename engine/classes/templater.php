b95b32ae9151d656dd553dfb57f1d8852ce6e7a5
Boris Stelmah <mikael.chip@gmail.com>
2015-04-05 19:01:58 +0300

    added templater class

---
+++ engine/classes/templater.php
@@ -0,0 +1,17 @@
+<?php
+
+class Templater {
+	public static function render($file, $defines = array()) {
+		$path = SITE_ROOT. "/engine/template/". $file;
+		if(!file_exists($path)) { return false; }
+		$handle = fopen($path, "r");
+		$data = fread($handle, filesize($path));
+		fclose($handle);
+		foreach($defines as $option => $value) {
+			$data = str_replace($option, $value, $data);
+		}
+		return $data;
+	}
+}
+
+?>
