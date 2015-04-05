3cdb09fb957be830421936d465fb79b7a05d07b4
Boris Stelmah <mikael.chip@gmail.com>
2015-04-05 18:54:19 +0300

    changed default index page

--- engine/controllers/index.php
+++ engine/controllers/index.php
@@ -1,2 +1,20 @@
-﻿<h1>Hello, world!</h1>
-&copy RAPTOR LinkDetect, 2015
+<!DOCTYPE html>
+<html>
+<body>
+<table border=0 width=100% height=100%>
+<tr>
+	<td align=center><table border=0 cellpadding=0 cellspacing=0 width=300>
+	<tr><td bgcolor=#000000><table border=0 cellpadding=3 cellspacing=1 width=300>
+	<tr>
+		<td align=center bgcolor=#0000FF><font color=#FFFFFF face='Verdana, Arial, Helvetica, sans-serif' size=2><b>Hello, world!</b></font></td>
+	</tr>
+	<tr>
+		<td align=center bgcolor=#FFFFFF><font color=#000000 face='Verdana, Arial, Helvetica, sans-serif' size=2><b>This site is just created <br> If you read this message, you installed LinkDetect correctly. Good luck!</b></font></td>
+	</tr>
+	</table></td></tr></table>
+	<font color=gray face='Verdana, Arial, Helvetica, sans-serif' size=1>&copy; RAPTOR LinkDetect</font>
+	</td>
+</tr>
+</table>
+</body>
+</html>
