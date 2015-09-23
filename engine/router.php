35892135e4388f1354b7f0fd2e75a162de87dbca
Boris Stelmah <mikael.chip@gmail.com>
2015-09-23 07:37:52 +0300

    code edit under standarts

--- engine/router.php
+++ engine/router.php
@@ -1,39 +1,59 @@
 <?php
 
-class Router {
-    public static function Start() {
-    	$urlArray = @explode("/", str_replace($GLOBALS['config']['root'], "", $_SERVER["REDIRECT_URL"]));
-        if(!empty($urlArray[1])) { $dDriver = $urlArray[1]; } else { $dDriver = "index"; }
-        if(!empty($urlArray[2])) { $dAction = $urlArray[2]; } else { $dAction = "index"; }
-		
-		$GLOBALS['driver'] = $dDriver;
-		$GLOBALS['action'] = $dAction;
-		$GLOBALS['link']  = $urlArray;
-		
-        @include_once(SITE_ROOT. "/engine/controllers/". $dDriver .".php");
-		
-		if(NO_MVC == "no") {
-			
-			$controllerClassName = $dDriver . "Controller";
-			$actionMethod = "action". $dAction;
-			
-			if(!class_exists($controllerClassName)) {
-				header('HTTP/1.1 404 Not Found');
+	class Router 
+	{
+		public static function Start() 
+		{
+			$urlArray = @explode("/", str_replace($GLOBALS['config']['root'], "", $_SERVER["REDIRECT_URL"]));
+			if(!empty($urlArray[1])) 
+			{ 
+				$dDriver = $urlArray[1]; 
+			} 
+			else 
+			{ 
+				$dDriver = "index"; 
 			}
 			
-			$controllerClass = new $controllerClassName;
+			if(!empty($urlArray[2])) 
+			{ 
+				$dAction = $urlArray[2]; 
+			} 
+			else 
+			{ 
+				$dAction = "index"; 
+			}
 			
-			if(!method_exists($controllerClass, $actionMethod)) { 
-				header('HTTP/1.1 404 Not Found'); 
+			$GLOBALS['driver'] = $dDriver;
+			$GLOBALS['action'] = $dAction;
+			$GLOBALS['link']  = $urlArray;
+			
+			@include_once(SITE_ROOT. "/engine/controllers/". $dDriver .".php");
+			
+			if(NO_MVC == "no") 
+			{
+				
+				$controllerClassName = $dDriver . "Controller";
+				$actionMethod = "action". $dAction;
+				
+				if(!class_exists($controllerClassName))
+				{
+					header('HTTP/1.1 404 Not Found');
+				}
+				
+				$controllerClass = new $controllerClassName;
+				
+				if(!method_exists($controllerClass, $actionMethod)) 
+				{ 
+					header('HTTP/1.1 404 Not Found'); 
+				}
+				else {
+					$controllerClass->$actionMethod(); 
+				}
+			
 			}
-			else {
-				$controllerClass->$actionMethod(); 
-			}
-		
-		}
 
-		return 1;
-    }
-}
+			return 1;
+		}
+	}
 
 ?>
--- index.php
+++ index.php
@@ -4,13 +4,14 @@ session_start();
 
 define("WEBSITE", 1);
 define("SITE_ROOT", __DIR__);
-define("NO_MVC", "yes");	# Yes / No
+define("NO_MVC", "no");	# Yes / No
 
 error_reporting(0);
 
 require_once(SITE_ROOT. "/engine/inc/config.php");
 
 require_once(SITE_ROOT. "/engine/router.php");
+require_once(SITE_ROOT. "/engine/api.php");
 
 Router::Start();
 
